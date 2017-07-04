f1e7429eae58cff0178df777f3d2425e204f9dae
Neil Ferner <n.e.ferner@gmail.com>
2017-07-03 23:04:01 -0400

    add the Seat model and create css tables and chairs

---
+++ app/model/seat.js
@@ -0,0 +1,11 @@
+class Seat {
+  contructor({id, table, chair, first, last}){
+    this.id = id;
+    this.table = table;
+    this.chair = chair;
+    this.first = first;
+    this.last = last;
+  }
+}
+
+module.exports = Seat;
